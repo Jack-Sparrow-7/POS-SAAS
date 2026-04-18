3937413c946ab2e27fb04745f71f94ebbc494eff
Jeyanandh V S <155248243+Jack-Sparrow-7@users.noreply.github.com>
2026-04-19 00:49:45 +0530

    chore: add pubspec.yaml for project configuration and workspace setup

---
+++ packages/enums/example/enums_example.dart
@@ -0,0 +1,6 @@
+import 'package:enums/enums.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/validation/example/validation_example.dart
@@ -0,0 +1,6 @@
+import 'package:validation/validation.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
